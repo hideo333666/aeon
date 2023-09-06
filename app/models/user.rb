20a5dec13179c1f263e45b31bd3aeb33c5a2f84c
hideo333666 <muramura031409@gmail.com>
2023-09-06 03:26:13 +0000

    アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+        has_many :posts
+        has_many :tasks
+        has_many :notifications
+        has_many :achievements
+        has_many :contributions
+        has_many :notes
+        has_many :events
 end
