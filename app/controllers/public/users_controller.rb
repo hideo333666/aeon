dd6573e439918cfcab71a513f9e048af78d06700
hideo333666 <muramura031409@gmail.com>
2023-09-25 05:50:34 +0000

    マイページレイアウト完成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,6 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
 
   def show
     @user = User.find(params[:id])
