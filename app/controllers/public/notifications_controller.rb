120e4594ea5acaddf3fa00cfd0fdf4bb588bedbf
hideo333666 <muramura031409@gmail.com>
2023-09-29 08:46:40 +0000

    通知機能作成中

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -2,6 +2,18 @@ class Public::NotificationsController < ApplicationController
   
   def index
     @notifications = current_user.notifications.order(created_at: :desc)
-    render json: { count: @notifications.count }
+    render json: @notifications
   end
+  
+  def unread_count
+    count = current_user.notifications.unread.count
+    render json: { count: count }
+  end
+  
+  def mark_as_read
+    current_user.notifications.unread.update_all(read: true)
+    head :ok
+  end
+  
 end
+
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,8 @@
 class Notification < ApplicationRecord
   belongs_to :user
+  scope :unread, -> { where(read: false) }
+  
   validates :message, presence: true
   validates :time, presence: true
+  
 end
