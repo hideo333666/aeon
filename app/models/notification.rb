369e5d352f5ec204651c1b39bad810890e4b8881
hideo333666 <muramura031409@gmail.com>
2023-10-08 16:25:27 +0000

    通知メッセージ変更

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,11 @@ class Notification < ApplicationRecord
   belongs_to :user
   scope :unread, -> { where(read: false) }
   
+  def message
+    task_name = super.split("Task ").last.split(" is").first
+    "タスクの期限がきています\n\t#{task_name}"
+  end
+  
   validates :message, presence: true
   validates :time, presence: true
   
